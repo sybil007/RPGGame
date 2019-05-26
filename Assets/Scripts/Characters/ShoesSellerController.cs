7221e4c22ff1b578b343faae187edf9428763e4e
Paulina S <paulina.syposz@gmail.com>
2019-05-26 19:02:48 +0200

    Added graveyard localisation

---
+++ Assets/Scripts/Characters/ShoesSellerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoesSellerController : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+		animator.SetInteger("Type", (int)IdlingType.Afraid);
+	}
+}
